b0cd6e12fc1ec220d8f6bcae6f7155c486471e79
henry <henrychenchn@gmail.com>
2020-03-07 18:06:22 +0800

    update

--- rename_jpg_file_by_shooting_date.py
+++ rename_jpg_file_by_shooting_date.py
@@ -16,39 +16,39 @@ count = 0
 pass_count = 0
 
 for file in dir_list:
-    if file.lower().endswith('.jpg') | file.lower().endswith('.jpeg'):
-        f = open(path + file, 'rb')
-        tags = exifread.process_file(f, stop_tag='DateTimeOriginal', details=False)
-        f.close()
-
-        try:
-            shooting_date = tags['EXIF DateTimeOriginal']
-        except KeyError:
-            print("Pass: " + file)
-            pass_count = pass_count + 1
-            continue
-
-        dst_name = str(shooting_date).replace(":", "").replace(" ", "_")
-
-        random_int = str(random.randint(1000, 9999))
-
-        src_extension = os.path.splitext(file)[1].lower()
-
-        dst_name = dst_name + "_" + str(random_int) + src_extension
-
-        while True:
-            try:
-                os.rename(path + file, path + dst_name)
-                break
-            except FileExistsError:
-                dst_name.replace(random_int, str(random.randint(1000, 9999)))
-                os.rename(path + file, path + dst_name)
-
-        count = count + 1
-    else:
+    if not file.lower().endswith('.jpg') | file.lower().endswith('.jpeg'):
         print("Pass: " + file)
         pass_count = pass_count + 1
         continue
+
+    f = open(path + file, 'rb')
+    tags = exifread.process_file(f, stop_tag='DateTimeOriginal', details=False)
+    f.close()
+
+    try:
+        shooting_date = tags['EXIF DateTimeOriginal']
+    except KeyError:
+        print("Pass: " + file)
+        pass_count = pass_count + 1
+        continue
+
+    dst_name = str(shooting_date).replace(":", "").replace(" ", "_")
+
+    random_int = str(random.randint(1000, 9999))
+
+    src_extension = os.path.splitext(file)[1].lower()
+
+    dst_name = dst_name + "_" + str(random_int) + src_extension
+
+    while True:
+        try:
+            os.rename(path + file, path + dst_name)
+            break
+        except FileExistsError:
+            dst_name.replace(random_int, str(random.randint(1000, 9999)))
+            os.rename(path + file, path + dst_name)
+
+    count = count + 1
 
 print("Renamed: " + str(count))
 print("Passed: " + str(pass_count))
--- rename_mp4_mov_file_by_shooting_date.py
+++ rename_mp4_mov_file_by_shooting_date.py
@@ -16,55 +16,54 @@ count = 0
 pass_count = 0
 
 for file in dir_list:
-    low = file.lower()
-    if low.endswith('mp4') | low.endswith('mov'):
-        media_info = MediaInfo.parse(path + file)
-
-        for track in media_info.tracks:
-            if track.track_type == 'General':
-
-                try:
-                    tagged_date = track.tagged_date[4:]
-                except TypeError:
-                    print("Pass: " + file)
-                    pass_count = pass_count + 1
-                    continue
-
-                tagged_date = tagged_date.replace("-", "").replace(" ", "_").replace(":", "")
-
-                # hour + 8
-                hour = tagged_date[9:11]
-                hour = int(hour) + 8
-                if hour < 10:
-                    hour = str("0" + str(hour))
-                else:
-                    hour = str(hour)
-
-                str_list = list(tagged_date)
-                str_list[9] = hour[0]
-                str_list[10] = hour[1]
-                tagged_date = "".join(str_list)
-
-                random_int = str(random.randint(1000, 9999))
-
-                src_extension = os.path.splitext(file)[1].lower()
-
-                dst_name = tagged_date + "_" + str(random_int) + src_extension
-
-                while True:
-                    try:
-                        os.rename(path + file, path + dst_name)
-                        break
-                    except FileExistsError:
-                        dst_name.replace(random_int, str(random.randint(1000, 9999)))
-                        os.rename(path + file, path + dst_name)
-
-                count = count + 1
-                break
-    else:
+    if not file.lower().endswith('mp4') | file.lower().endswith('mov'):
         print("Pass: " + file)
         pass_count = pass_count + 1
         continue
+        
+    media_info = MediaInfo.parse(path + file)
+
+    for track in media_info.tracks:
+        if track.track_type == 'General':
+
+            try:
+                tagged_date = track.tagged_date[4:]
+            except TypeError:
+                print("Pass: " + file)
+                pass_count = pass_count + 1
+                continue
+
+            tagged_date = tagged_date.replace("-", "").replace(" ", "_").replace(":", "")
+
+            # hour + 8
+            hour = tagged_date[9:11]
+            hour = int(hour) + 8
+            if hour < 10:
+                hour = str("0" + str(hour))
+            else:
+                hour = str(hour)
+
+            str_list = list(tagged_date)
+            str_list[9] = hour[0]
+            str_list[10] = hour[1]
+            tagged_date = "".join(str_list)
+
+            random_int = str(random.randint(1000, 9999))
+
+            src_extension = os.path.splitext(file)[1].lower()
+
+            dst_name = tagged_date + "_" + str(random_int) + src_extension
+
+            while True:
+                try:
+                    os.rename(path + file, path + dst_name)
+                    break
+                except FileExistsError:
+                    dst_name.replace(random_int, str(random.randint(1000, 9999)))
+                    os.rename(path + file, path + dst_name)
+
+            count = count + 1
+            break
 
 print("Renamed: " + str(count))
 print("Passed: " + str(pass_count))
